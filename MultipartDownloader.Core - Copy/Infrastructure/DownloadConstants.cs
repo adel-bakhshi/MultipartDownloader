219100791a15355321cfe7e225be34e8efc51c4d
ADdy2142 <adelbakhshi78@yahoo.com>
2025-04-14 11:24:40 +0330

    Remove copied project

--- MultipartDownloader.Core - Copy/Infrastructure/DownloadConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MultipartDownloader.Core.Infrastructure;
-
-public static class DownloadConstants
-{
-    public const long ProgressThreshold = 1024 * 1024; // 1 MB
-    public const int NumberOfSpeedSamples = 101;
-    public const int NumberOfMedianSamples = 100;
-}
